56ec6eea3e0dc2e502a379d50bb0ef452b72df65
Alex Ezechi <alex_ezechi@outlook.com>
2024-04-24 22:44:20 +0100

    Imported the new post function

--- src/SocialFeed.js
+++ src/SocialFeed.js
@@ -4,25 +4,42 @@ import axios from 'axios'
 
 const SocialFeed = () => {
   const [newPost, setNewPost] = useState([])
-  const [displayposts, setdisplayposts] = useState([])
+  const [displayPosts, setdisplayPosts] = useState([])
+
+// Fetching posts from the server
 
   const FetchPosts = async () => {
 
     try{
       const response = await axios.get('/api/posts.json')
-      setdisplayposts(response.data.posts)
+      setdisplayPosts(response.data.posts)
     } catch (error) {
       console.log(`Failed to load posts: ${error}`)
     }
   }
 
+
+// Posting new posts to the server by creating a new post array and updating the displayPosts state
+
+  const handleSubmit = async (e) => {
+    const post = newPost;
+    const updatedPosts = [...displayPosts]
+
+    updatedPosts.unshift({name: 'Alex Ezechi', post: post})
+    setdisplayPosts(updatedPosts)
+
+    setNewPost('')
+
+    e.preventDefault()
+    console.log(`Publishing post: ${post}`)
+    console.log(displayPosts)
+  }
+
+// Using the useEffect hook to fetch posts from the server when the component mounts
+
   useEffect(() => {
     FetchPosts()
   }, [])
-
-// First create an empty array to store the posts.
-// Then make it so that when the user presses the post button, the new post is added to the array
-// In the return, map through the array and display each post in a div
   
 
 
@@ -46,13 +63,16 @@ const SocialFeed = () => {
                   <a href="">Tag Friends</a>
                   <a href="">Feeling/Activity</a>
                   <a href="">LinkedIn</a>
-                  <button className="post">Post</button>
+                  <button className="post"
+                  onClick={handleSubmit}
+                  
+                  >Post</button>
               </nav>
       </div>
 
       <div className="postsFeed">Post Feed
       {
-        displayposts.map((posts) => (
+        displayPosts.map((posts) => (
           <div className="newFeed">
             <h2>{posts.name}</h2>
             <p>{posts.post}</p>
